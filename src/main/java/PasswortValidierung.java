9951ca127a39cd889f6ccfcf7826f104e7c7124f
sebastianwitt <witt.seb@outlook.de>
2022-09-02 16:36:27 +0200

    PasswortValidierung Projekt

--- src/main/java/PasswortValidierung.java
+++ src/main/java/PasswortValidierung.java
@@ -1,39 +1,88 @@
-public class PasswortValidierung {//    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    //        String password = "pass";
-//        getLength(password);
-//    }
-    public static int getLength(String password) {
+public class Passwortvaliedierung {
+    public static void main(String[] args) {
+        System.out.println("Geben Sie ein Passwort ein:");
+        Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+        System.out.println(checkPassword(password));
+    }
+    public static int getLength(String password){
         int length = password.length();
         return length;
     }
 
-    public static String checkLength(String password) {
-        int length = getLength(password);
+    public static String checkPassword(String password){
         String Message = null;
+        boolean isPasswordBanned = isPasswordBanned(password);
+        int length = getLength(password);
         boolean containsNumber = checkNumber(password);
-        if (length >= 8 && containsNumber) {
-            Message = "Passwort akzeptiert";
-        } else if (length >= 8 && !containsNumber) {
-            Message = "Fehlende Zahl im Passwort.";
-        } else {
-            Message = "Passwort zu kurz!";
+        boolean containUpAndLow = checkUpperLower(password);
+
+        if(isPasswordBanned){
+            Message = "Passwort verboten.";
+        }else{
+            if(length >=8 && containsNumber && containUpAndLow){
+                Message = "Passwort akzeptiert.";
+            }else if (length >=8 && containsNumber && !containUpAndLow) {
+                Message = "Fehlende Groß- und Kleinschreibung im Passwort.";
+            }else if (length >= 8 && !containsNumber && containUpAndLow) {
+                Message = "Fehlende Zahl im Passwort.";
+            }else if (length >=8 && !containsNumber && !containUpAndLow) {
+                Message = "Fehlende Zahl,  Groß- und Kleinschreibung im Passwort.";
+            }else{
+                Message = "Passwort zu kurz!";
+            }
         }
         return Message;
     }
 
-    public static boolean checkNumber(String password) {
-        boolean containsNumber = true;
-        int number[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 0; i < number.length; i++) {
-            if (password.contains(String.valueOf(number[i]))) {
+    public static boolean checkNumber(String password){
+        boolean containsNumber=true;
+        String numbers [] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for(int i = 0; i<numbers.length; i++){
+            if(password.contains(numbers[i])){
                 containsNumber = true;
                 break;
-            } else {
+            }else {
                 containsNumber = false;
             }
         }
-            return containsNumber;
+        return containsNumber;
+    }
 
+    public static boolean checkUpperLower(String password){
+        boolean containUp = true;
+        boolean containLow = true;
+        char[] c = password.toCharArray();
+        for(int i=0; i<c.length; i++){
+            if(Character.isUpperCase(c[i])){
+                containUp = true;
+                break;
+            }else{
+                containUp = false;
+            }
+        }
+
+        for(int i=0; i<c.length; i++){
+            if(Character.isLowerCase(c[i])){
+                containLow = true;
+                break;
+            }else{
+                containLow = false;
+            }
+        }
+        return containLow && containUp;
+    }
+
+    public static boolean isPasswordBanned(String password){
+        ArrayList<String> BannedPasswords = new ArrayList<>();
+        BannedPasswords.add("Password1");
+        BannedPasswords.add("Password2");
+        BannedPasswords.add("ABcd1234");
+        BannedPasswords.add("Ab123456");
+
+        return BannedPasswords.contains(password);
     }
 }
--- src/test/java/PasswortValiedierungTest.java
+++ src/test/java/PasswortValiedierungTest.java
@@ -2,50 +2,152 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PasswortValiedierungTest{
-
-        @Test
-        void getLengthTest() {
+@Test
+    void getLengthTest(){
             // GIVEN
             String password = "password";
             // WHEN
-            int actual = PasswortValidierung.getLength(password);
+            int actual = Passwortvalidierung.getLength(password);
 
             // THEN
             assertEquals(8, actual);
-        }
+            }
 
-        @Test
-        void checkLengthTestForLonger8() {
+@Test
+    void checkPasswordTestForForbiddenPassword(){
+            // GIVEN
+            String password = "Password1";
+            // WHEN
+            String actual = Passwortvalidierung.checkPassword(password);
+
+            // THEN
+            assertEquals("Passwort verboten.", actual);
+            }
+
+@Test
+    void checkPasswordTestForLonger8NumberUpperLower(){
+            // GIVEN
+            String password = "paSsword1";
+            // WHEN
+            String actual = Passwortvalidierung.checkPassword(password);
+
+            // THEN
+            assertEquals("Passwort akzeptiert.", actual);
+            }
+@Test
+    void checkPasswordTestForLonger8NoAlphabets(){
+            // GIVEN
+            String password = "12345678";
+            // WHEN
+            String actual = Passwortvalidierung.checkPassword(password);
+
+            // THEN
+            assertEquals("Fehlende Groß- und Kleinschreibung im Passwort.", actual);
+            }
+
+@Test
+    void checkPasswordTestForLonger8NumberNoUpperLower(){
             // GIVEN
             String password = "password1";
             // WHEN
-            String actual = PasswortValidierung.checkLength(password);
+            String actual = Passwortvalidierung.checkPassword(password);
 
             // THEN
-            assertEquals("Passwort akzeptiert", actual);
-        }
-
-        @Test
-        void checkLengthTestForShorter8() {
+            assertEquals("Fehlende Groß- und Kleinschreibung im Passwort.", actual);
+            }
+@Test
+    void checkPasswordTestForLonger8NoNumberNoUpperLower(){
             // GIVEN
-            String password = "pass";
+            String password = "password";
             // WHEN
-            String actual = PasswortValidierung.checkLength(password);
+            String actual = Passwortvalidierung.checkPassword(password);
+
+            // THEN
+            assertEquals("Fehlende Zahl,  Groß- und Kleinschreibung im Passwort.", actual);
+            }
+@Test
+    void checkPasswordTestForShorter8(){
+            // GIVEN
+            String password = "pass123";
+            // WHEN
+            String actual = Passwortvalidierung.checkPassword(password);
 
             // THEN
             assertEquals("Passwort zu kurz!", actual);
-        }
+            }
 
-        @Test
-        void checkNumberTestforNoNumber() {
+@Test
+    void checkNumberTestFor0(){
             // GIVEN
-            String password = "passwort";
-
+            String password = "password0";
             // WHEN
-            boolean actual = PasswortValidierung.checkNumber(password);
+            boolean actual = Passwortvalidierung.checkNumber(password);
+
+            // THEN
+            assertEquals(true, actual);
+            }
+@Test
+    void checkNumberTestFor10(){
+            // GIVEN
+            String password = "password10";
+            // WHEN
+            boolean actual = Passwortvalidierung.checkNumber(password);
+
+            // THEN
+            assertEquals(true, actual);
+            }
+@Test
+    void checkNumberTestForNoNumber(){
+            // GIVEN
+            String password = "password";
+            // WHEN
+            boolean actual = Passwortvalidierung.checkNumber(password);
 
             // THEN
             assertEquals(false, actual);
-        }
-    }
+            }
+
+@Test
+    void checkUpperLowerTest(){
+            // GIVEN
+            String password = "passWord";
+            // WHEN
+            boolean actual = Passwortvalidierung.checkUpperLower(password);
+
+            // THEN
+            assertEquals(true, actual);
+            }
+
+@Test
+    void checkUpperLowerTestOnlyUpper(){
+            // GIVEN
+            String password = "PASSWORD";
+            // WHEN
+            boolean actual = Passwortvalidierung.checkUpperLower(password);
+
+            // THEN
+            assertEquals(false, actual);
+            }
+
+@Test
+    void checkUpperLowerTestOnlyLower(){
+            // GIVEN
+            String password = "password";
+            // WHEN
+            boolean actual = Passwortvalidierung.checkUpperLower(password);
+
+            // THEN
+            assertEquals(false, actual);
+            }
+
+@Test
+    void isPasswordBannedTest(){
+            // GIVEN
+            String password = "Password1";
+            // WHEN
+            boolean actual = Passwortvalidierung.isPasswordBanned(password);
+
+            // THEN
+            assertEquals(true, actual);
+            }
+            }
